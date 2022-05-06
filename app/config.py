a737288b4dcaca6ea92a99f10188da1d073a3484
kajonsak <kajonsak.c@blockfint.com>
2022-05-06 17:53:13 +0700

    EDIT: ENV

--- app/config.py
+++ app/config.py
@@ -3,12 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv('DB_USER')
-DB_PASS = os.getenv('DB_PASS')
-DB_NAME = os.getenv('DB_NAME')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_INSTANCE_NAME = os.getenv('DB_INSTANCE_NAME')
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
 # ON client
 postgresqlConfig = "postgresql+pg8000://{}:{}@{}:{}/{}".format(
